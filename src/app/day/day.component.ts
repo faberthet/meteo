03e61f723728853b5dfd57d5ada466328c9bca33
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2023-01-24 16:25:34 +0100

    organisation des données meteo

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faCloud,faSun,faCloudSun,faCloudShowersHeavy,faCloudSunRain, faSmog, faSnowflake, faCloudBolt } from '@fortawesome/free-solid-svg-icons';
+import { Icon_map } from '../icon-map';
 
 @Component({
   selector: 'app-day',
@@ -9,5 +10,22 @@ import { faCloud } from '@fortawesome/free-solid-svg-icons';
 export class DayComponent {
   @Input() day!:string;
   faCloud=faCloud;
+  faSun=faSun
+  faCloudSun=faCloudSun
+  faRain=faCloudShowersHeavy
+  faCloudSunRain=faCloudSunRain
+  faSmog=faSmog
+  faSnowflake=faSnowflake
+  faCloudBolt=faCloudBolt
+  /*
+  0,1: faSun
+  2: faCloudSun
+  3:faCloud
+  45,48:faSmog
+  51,53,55,56,57,61,63,65,66,67,80,81,82: faRain
+  71,73,75,77,85,86:faSnowflake
+  95,96,99:faCloudBolt
+
+  */
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -50,7 +50,7 @@ export class WeatherService {
   parseDailyWeather({daily}:{daily:any}){
     return daily.time.map((time:number,index:number)=>{
       return {
-        timestamp: time * 1000,
+        day: new Date(time * 1000).toLocaleDateString('fr-FR', { weekday: 'long' }),
         maxTemp: Math.round(daily.temperature_2m_max[index]),
         minTemp: Math.round(daily.temperature_2m_min[index]),
         iconCode: daily.weathercode[index]
@@ -61,7 +61,9 @@ export class WeatherService {
   parseHourlyWeather({hourly, current_weather}:{hourly:any, current_weather:any}){
     return hourly.time.map((time:number,index:number)=>{
       return {
-        timestamp: time * 1000,
+        timestamp:time*1000,
+        day: new Date(time * 1000).toLocaleDateString('fr-FR', { weekday: 'long' }),
+        hour: new Date(time * 1000).toLocaleTimeString('fr-FR', { hour: 'numeric' }).replace(/^0(?:0:0?)?/, ''),
         temp: Math.round(hourly.temperature_2m[index]),
         flTemp: Math.round(hourly.apparent_temperature[index]),
         iconCode: hourly.weathercode[index],
