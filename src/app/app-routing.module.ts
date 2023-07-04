be99732e1cfcdb84c684a9ce5315ae2b8775ad82
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2023-07-05 00:27:26 +0200

    autocomplete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'search', component: AutocompleteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -10,18 +12,26 @@ import { HoursComponent } from './hours/hours.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { WeatherService } from './services/weather.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './modules/material/material.module';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DayComponent,
-    HoursComponent
+    HoursComponent,
+    AutocompleteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
